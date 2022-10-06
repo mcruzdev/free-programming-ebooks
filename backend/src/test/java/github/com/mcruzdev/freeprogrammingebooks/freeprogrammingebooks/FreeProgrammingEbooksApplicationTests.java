efd80a1197437817f2df64eef6413f297f9516b9
Matheus Cruz <oic_matsousa@uolinc.com>
2022-10-06 13:17:31 -0300

    chore: add java and springboot backend

---
+++ backend/src/test/java/github/com/mcruzdev/freeprogrammingebooks/freeprogrammingebooks/FreeProgrammingEbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.mcruzdev.freeprogrammingebooks.freeprogrammingebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeProgrammingEbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
